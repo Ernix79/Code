c8c4f598f517894ebf16717fe8816e1a6d4642a4
Ernix_79 <129004658+Ernix79@users.noreply.github.com>
2024-10-03 11:33:41 -0400

    Add files via upload

---
+++ JS/base js/Fonction/fonction.js
@@ -0,0 +1,10 @@
+function multiplication(x,y){
+    return x * y ;
+    alert("Ce message ne serra jamais afficher")
+}
+
+let resultat1 = multiplication(5,10);
+let resultat2 = multiplication(-4,60);
+
+resultat1 += 2;
+alert(resultat1);
